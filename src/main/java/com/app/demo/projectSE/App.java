62efc93ee4a72c0c3a81c1104ba0c7fe64dbb007
Vandhithapushpala <pushpalavandhitha@gmail.com>
2024-01-02 12:55:31 +0530

    demo

---
+++ src/main/java/com/app/demo/projectSE/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.projectSE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
